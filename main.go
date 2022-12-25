b0db338a6fd4f3b924c3f16c88669a4c8a46e714
SoGreatAndPowerful <sogreatandpowerful@tuta.io>
2022-12-26 03:35:51 +0600

    refactor main.go

--- main.go
+++ main.go
@@ -7,19 +7,29 @@ import (
 )
 
 func main() {
-	// Specify the host and port to scan
+	// Declare variables to hold the host and port to scan
 	var host string
-	fmt.Print("Enter the domain: ")
-	fmt.Scanln(&host)
-
 	var port int
+
+	// Read the host and port from user input
+	fmt.Print("Enter the domain: ")
+	_, err := fmt.Scanln(&host)
+	if err != nil {
+		fmt.Println("Error reading host:", err)
+		return
+	}
+
 	fmt.Print("Enter Port number: ")
-	fmt.Scanln(&port)
+	_, err = fmt.Scanln(&port)
+	if err != nil {
+		fmt.Println("Error reading port:", err)
+		return
+	}
 
 	// Create a target address by combining the host and port
 	target := fmt.Sprintf("%s:%d", host, port)
 
-	// Create a TCP connection to the target address
+	// Create a TCP connection to the target address with a timeout of 5 seconds
 	conn, err := net.DialTimeout("tcp", target, time.Second*5)
 	if err != nil {
 		// An error occurred, which means the port is probably closed
@@ -28,6 +38,6 @@ func main() {
 		// No error occurred, which means the port is probably open
 		fmt.Printf("Port %d is open\n", port)
 		// Don't forget to close the connection when you're done
-		conn.Close()
+		defer conn.Close()
 	}
 }
